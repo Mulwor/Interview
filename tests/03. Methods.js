f4551dc3a03ff7bf9c672b285d9a18276e1a3df7
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-22 19:02:12 +0300

    feat: new question

---
+++ tests/03. Methods.js
@@ -0,0 +1,17 @@
+// ! String method
+const str = 'test';
+str.padStart(10); // "      test"
+str.padStart(10, '-'); // "------test"
+str.padEnd(10); // "test      "
+str.padEnd(10, '-'); // "test------"
+
+const str1 = '  test   ';
+str1.trim(); // "test"
+
+const str2 = 'Hello, John'; // 11 символов
+str2.startsWith('John'); // False
+str2.startsWith('John', 7); // True (начиная с 7 символа начинается John)
+
+str2.endsWith('John'); // True
+str2.endsWith('Hello', 5); // True
+str2.endsWith('John', 11); // True
