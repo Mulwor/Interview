0cbcaac948355b73d087a8eadc0b612fe1c3193c
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-28 12:47:36 +0300

    feat: code

--- tests/02. Loops.js
+++ tests/02. Loops.js
@@ -1,3 +1,32 @@
+// Циклы и инструкции: 
+
+
+
+// ! switch(), case .., break,
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Маловато' );
+    break;
+  case 4:
+    alert( 'В точку!' );
+    break;
+  case 5:
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( "Нет таких значений" );
+}
+
+
+
+
+
+
+
+
+
 
 
 
