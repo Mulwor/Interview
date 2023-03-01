55985ca29f50bd8d756099b7d4d435442ac6eda4
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-01 12:27:31 +0300

    feat: изменения содержимых папок

--- tests/II. operators/operators.js
+++ tests/II. operators/operators.js
@@ -1,45 +1,3 @@
-console.log("---- Простая проверка----")
-
-console.log(typeof 0)                        
-console.log(typeof true)                     
-console.log(typeof 'Javascript')             
-console.log(typeof undefined)               
-console.log(typeof Math)                     
-console.log(typeof Symbol('JS'))             
-console.log(typeof null)                    
-console.log(typeof function() {})            
-console.log(typeof NaN)                      
-console.log(typeof typeof 100)               
-
-
-console.log("---- Явное приведение типов, а также thruthy/fasly----")
-
-console.log(Boolean(''))                   
-console.log(Boolean('Hello'))               
-console.log(Boolean(' '))                 
-console.log(Boolean('0'))                 
-console.log(Boolean(0))                
-console.log(Boolean(1))                   
-console.log(Boolean(null))                 
-console.log(Boolean(undefined))            
-console.log(Boolean(false))                
-console.log(Boolean([]))                  
-console.log(Boolean({}))                  
-console.log(Boolean(Symbol()))            
-console.log(Boolean(function(){}))        
-
-console.log(Number(" 123 "))             
-console.log(Number("123z"))              
-console.log(Number(true))              
-console.log(Number(false))             
-console.log(Number("\n"))
-
-console.log(String(123))                   
-console.log(String(-12.3))                  
-console.log(String(null))                   
-console.log(String(undefined))             
-
-
 console.log(' ---- Строгое равенство и нестрогое равенство -----')
 
 console.log(2 == '2')             
@@ -100,6 +58,9 @@ console.log({} + [])
 console.log({}+[]+{}+[1])          
 console.log(new Date(0) - 0)      
 console.log(new Date(0) + 0)       
+
+
+
 console.log('------ Логические операторы -------')
 console.log(0 || "0" && {})        
 console.log(!+[])                 
@@ -110,12 +71,3 @@ console.log(!+[]+[]+![])
 
 console.log(+true)
 console.log(!"John")
-
-
-let a = 3
-let b = new Number(3)          
-let c = 3
-
-console.log(a == b)     
-console.log(a === b)    
-console.log(b === c)
