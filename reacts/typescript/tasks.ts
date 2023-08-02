51c6fbeed777473eac6bdf798547b612ceda5e3d
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-08-02 15:15:20 +0300

    start: section

--- reacts/typescript/tasks.ts
+++
@@ -1,8 +0,0 @@
-import { Equal, Expect, NotAny } from '@type-challenges/utils'
-
-type HelloWorld = string // expected to be a string
-
-type cases = [
-  Expect<NotAny<HelloWorld>>,
-  Expect<Equal<HelloWorld, string>>
-]
