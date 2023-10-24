02fcb75dbf358adcb413f3570436c2404024a684
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-10-24 10:53:22 +0300

    Update roadmap.js

--- roadmap.js
+++ roadmap.js
@@ -1,4 +1,8 @@
-// Roadmap(https://neetcode.io/roadmap)
+// Roadmap => https://neetcode.io/roadmap
 
-// Array - 
-// У массива есть ряд методов: 
+// Array (массив) - это глобальный объект, который предназначен для работы со списком. Например: список товаров (продуктов),
+let shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+console.log(shopping);
+// Если мы хотим получить товар, то можем использовать: shopping[0], а если заменить его, то можем использовать: shopping[0] = "tahini";
+
+
