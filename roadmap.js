4bc1d39b2e0c8e205c8cc9b6821ba2224a81b0a1
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-10-24 11:00:12 +0300

    Delete roadmap.js

--- roadmap.js
+++
@@ -1,8 +0,0 @@
-// Roadmap => https://neetcode.io/roadmap
-
-// Array (массив) - это глобальный объект, который предназначен для работы со списком. Например: список товаров (продуктов),
-let shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
-console.log(shopping);
-// Если мы хотим получить товар, то можем использовать: shopping[0], а если заменить его, то можем использовать: shopping[0] = "tahini";
-
-
